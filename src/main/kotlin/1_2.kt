bba8e78716724b10ba17f88da67b0eb981c6c883
wojciechwasik <32146328+wojciechwasik@users.noreply.github.com>
2019-12-11 09:07:18 +0100

    solution template

---
+++ src/main/kotlin/1_2.kt
@@ -0,0 +1,26 @@
+//package aocXXXX.dayXX
+
+import util.*
+
+//
+// Solution to problem 1
+// Solution to problem 2
+//
+
+fun main(args: Array<String>) {
+    val input = InputReader("/aocXXXX/dayXX.txt").readLines()
+
+    problem1(input)
+
+    problem2(input)
+}
+
+private fun problem1(input: List<String>) {
+
+    println("Problem 1 solution: ")
+}
+
+private fun problem2(input: List<String>) {
+
+    println("Problem 2 solution: ")
+}
