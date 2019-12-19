2a6dc34fbc6f87ade430aaba965e0591cc9566de
Wojciech Wasik <wojciech.wasik@ocado.com>
2019-12-19 10:33:31 +0100

    2019 day 19, part 1

---
+++ src/main/kotlin/aoc2019/day19.kt
@@ -0,0 +1,30 @@
+package aoc2019
+
+import util.*
+
+//
+// Area of tractor beam: 226
+// Problem 2 solution: 
+//
+
+fun main(args: Array<String>) {
+    val input = InputReader("/aoc2019/day19.txt").readSequence(',') { it.toLong() }
+
+    problem1(input)
+
+    problem2(input)
+}
+
+private fun problem1(input: List<Long>) {
+    var count = 0
+    for (x in 0..49)
+        for (y in 0..49)
+            if (Intcode(input).run(listOf(x.toLong(), y.toLong()))[0] == 1L) count++
+
+    println("Area of tractor beam: ${count}")
+}
+
+private fun problem2(input: List<Long>) {
+
+    println("Problem 2 solution: ")
+}
